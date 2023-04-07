f8f706ae6f66724deb893f06727b5e0e3ab154c0
you know whom <lianghengwei2011@163.com>
2023-04-07 11:21:50 +0800

    改用queryselectorall

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var divs = $('.cur-list div.duration');
+var divs = document.querySelectorAll('.cur-list div.duration');
 var timearr = [];
-divs.each((i,e) => {
+for (const e of divs) {
     timearr.push(e.innerText)
-});
+}
+
 var timeInSecond = []
 timearr.forEach((e) => {
     var time = e.split(':');
@@ -11,15 +12,18 @@ timearr.forEach((e) => {
     var st = m * 60 + s;
     timeInSecond.push(st);
 })
-var tts = timeInSecond.reduce((pv, c,i) =>{
+var tts = timeInSecond.reduce((pv, c, i) => {
     return pv + c
-},0)
-var hours = tts / 60 /60;
-console.log(hours,'小时')
+}, 0)
+var hours = tts / 60 / 60;
+console.log(hours, '小时')
 
 // 视频标题
 var titles = ''
-jQuery('.part').each((i,e)=>{titles += e.innerText + '\n'})
+var parts = document.querySelectorAll('.part')
+for (const e of parts) {
+    titles += e.innerText + '\n'
+}
 var e = document.createElement('div')
 e.innerText = titles;
-document.body.insertBefore(e,document.getElementById('app'))
+document.body.insertBefore(e, document.getElementById('app'))
